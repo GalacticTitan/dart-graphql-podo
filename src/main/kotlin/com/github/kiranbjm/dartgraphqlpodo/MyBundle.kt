66d13d9893f99e32d24b3f7515278f27223b34a0
GitHub Action <action@github.com>
2021-06-10 14:41:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/kiranbjm/dartgraphqlpodo/MyBundle.kt
+++ src/main/kotlin/com/github/kiranbjm/dartgraphqlpodo/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kiranbjm.dartgraphqlpodo
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kiranbjm/dartgraphqlpodo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kiranbjm/dartgraphqlpodo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kiranbjm.dartgraphqlpodo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kiranbjm.dartgraphqlpodo.MyBundle
 
 class MyApplicationService {
 
